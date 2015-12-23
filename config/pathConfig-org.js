56fbfd851b0df7bd34f33c0de882a9c8918b74d3
Babak Ommani <babak.ommani@marintek.sintef.no>
2015-12-23 15:52:59 +0100

    default location.

--- config/pathConfig-org.js
+++ config/pathConfig-org.js
@@ -3,6 +3,6 @@ path = require('path');
 exports.outPath = path.resolve(path.join(__dirname, '..', 'example'));
 exports.modelsPaths = [path.resolve(path.join(__dirname, '..', 'example', 'models'))];
 
-exports.codeGenerators = {'python': {'outPath': 'lang/python'},
-				 		  'matlab': {'outPath': 'lang/matlab'},
-				 		  'fortran': {'outPath': 'lang/fortran'}}
+exports.codeGenerators = {'python': {'outPath': 'lang/python/models'},
+				 		  'matlab': {'outPath': 'lang/matlab/models'},
+				 		  'fortran': {'outPath': 'lang/fortran/models'}}
